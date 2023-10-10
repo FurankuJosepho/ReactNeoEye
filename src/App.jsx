83b615dfda819189bd06410fe6b112c145a674de
FurankuJosepho <frank21garcia29@gmail.com>
2023-10-11 00:11:11 +0800

    books us added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./component/header";
 import Footer from './component/footer';
 import Home from "./pages/home";
+import Booking from "./pages/booking";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/app.css";
 import './css/footer.css'
@@ -16,7 +17,7 @@ function App() {
         <Route path="/" exact element={Home}/>
         <Route path="/Products"/>
         <Route path="/AboutUs"/>
-        <Route path="/Booking"/>
+        <Route path="/Booking" exact element={<Booking />}/>
         <Route path="/Login"/>
         <Route path="/YourAccount"/>
         <Route path="/Admin"/>
